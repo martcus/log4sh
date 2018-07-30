1ecc1a7c5f88c1576710d1a5d4f10358d6013623
martcus <marco.lovazzano@gmail.com>
2018-07-30 09:19:31 +0200

    Add usage function with custom help text, fix print to file

--- write_log/test_write_log.sh
+++ write_log/test_write_log.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source write_log.sh
 
-LOG_FILE=test
+LOG_LEVEL_DEFAULT="INFO"
 
 echo "--------" | write_log
 echo "This is a simple test to check the write_log function" | write_log
-echo "--------" | write_log
 echo "You can concat with command (ls | write_log)" | write_log
 ls | write_log
 echo "--------" | write_log
+echo "You can insert the level log" | write_log INFO
--- write_log/write_log.sh
+++ write_log/write_log.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 #--------------------------------------------------------------------------------------------------
 # write_log - Makes logging in Bash scripting suck less
-
-# LOG_DATE: if log date is not defined, use the format "+%Y-%m-%d %H:%M:%S
 # LOG_FILE: If log file is not defined, just echo the output
 #--------------------------------------------------------------------------------------------------
 
-#
+# Begin Global variables
+
+# End Global variables
+
+# Begin Help Section
+HELP_TEXT=""
+
+# This function is called in the event of an error.
+# Scripts which source this script may override by defining their own "usage" function
+usage() {
+    echo -e "${HELP_TEXT}";
+    exit 1;
+}
+# End Help Section
+
+# Begin Logging Section
 write_log()
 {
   while read text
@@ -18,7 +31,7 @@ write_log()
       # If log file is not defined, just echo the output
       if [ "$LOG_FILE" == "" ]; then
          if [ "$1" == "" ]; then echo $LOG_TIME": $text";
-         else  echo $LOG_TIME": $1 $text"; fi
+         else echo $LOG_TIME": [$1] $text"; fi
       else
         LOG=$LOG_FILE.`date +%Y%m%d`
         touch $LOG
@@ -26,7 +39,8 @@ write_log()
           echo $LOG_TIME": [ERROR] Cannot create log file $LOG. Exiting.";
           exit 1;
         fi
-        echo $LOG_TIME": $text" | tee -a $LOG;
+        echo $LOG_TIME": [$1] $text" | tee -a $LOG;
       fi
   done
 }
+# End Logging Section
