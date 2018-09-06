83b871a4561548c0086ded1bfa07c16fd6159ec8
Marco <marco.lovazzano@gmail.com>
2018-09-06 10:14:55 +0200

    Fix identation, getopt name

--- script/log4.sh
+++ script/log4.sh
@@ -10,42 +10,42 @@ LOG4SH_VERSION="2.0.1"
 
 # Internal function for logging
 function _log {
-  if [ $verb_lvl -le $verbosity ]; then
-    local level=${1:-}
-    local text=${2:-}
+    if [ $verb_lvl -le $verbosity ]; then
+        local level=${1:-}
+        local text=${2:-}
 
-    if [ ! -t 0 ]; then
-	  text=$(cat)" "${text}
+        if [ ! -t 0 ]; then
+            text=$(cat)" "${text}
+        fi
+
+        # if log date format is not defined, use the format "+%Y-%m-%d %H:%M:%S
+        LOG_TIME_FMT=${LOG_TIME_FMT:="+%Y-%m-%d %H:%M:%S"}
+        LOG_TIME=$(date "${LOG_TIME_FMT}")
+        LOG_FILE=${LOG_FILE:=""}
+
+        # if log file is not defined, just echo the output
+        if [ -z "$LOG_FILE" ]; then
+            _compose "${LOG_TIME}" "${level}" "${context[@]}" "${text}"
+        else
+            LOG=$LOG_FILE.$(date +%Y%m%d)
+            touch $LOG
+            if [ ! -f $LOG ]; then
+                _compose "${LOG_TIME}" "FATAL" "${context[@]}" "Cannot create log file $LOG. Exiting."
+                exit 1;
+            fi
+            _compose "${LOG_TIME}" "${level}" "${context[@]}" "${text}" | tee -a $LOG;
+        fi
     fi
-
-    # if log date format is not defined, use the format "+%Y-%m-%d %H:%M:%S
-    LOG_TIME_FMT=${LOG_TIME_FMT:="+%Y-%m-%d %H:%M:%S"}
-    LOG_TIME=$(date "${LOG_TIME_FMT}")
-    LOG_FILE=${LOG_FILE:=""}
-
-    # if log file is not defined, just echo the output
-    if [ -z "$LOG_FILE" ]; then
-      _compose "${LOG_TIME}" "${level}" "${context[@]}" "${text}"
-    else
-      LOG=$LOG_FILE.$(date +%Y%m%d)
-      touch $LOG
-      if [ ! -f $LOG ]; then
-        _compose "${LOG_TIME}" "FATAL" "${context[@]}" "Cannot create log file $LOG. Exiting."
-        exit 1;
-      fi
-      _compose "${LOG_TIME}" "${level}" "${context[@]}" "${text}" | tee -a $LOG;
-    fi
-  fi
 }
 
 # Internal function for compose the log message
 function _compose() {
-  local ltime=${1:-}
-  local level=${2:-}
-  local context=${3:-}
-  local text=${4:-}
+    local ltime=${1:-}
+    local level=${2:-}
+    local context=${3:-}
+    local text=${4:-}
 
-  echo -e "${ltime} ${level} ${context} ${text}";
+    echo -e "${ltime} ${level} ${context} ${text}";
 }
 
 # Begin Log commands
@@ -70,75 +70,75 @@ function TRACE() { verb_lvl=$trace_lvl   _log "TRACE  " "$@" ;}
 
 # Options command
 function _set_verbosity() {
-  case $1 in
-    FATAL)   verbosity=$fatal_lvl   ;;
-    ERROR)   verbosity=$error_lvl   ;;
-    WARNING) verbosity=$warning_lvl ;;
-    INFO)    verbosity=$info_lvl    ;;
-    DEBUG)   verbosity=$debug_lvl   ;;
-    TRACE)   verbosity=$trace_lvl   ;;
-    *)
-      echo -e "Error: $0 invalid option '$1'\nTry '$0 --help' for more information.\n"
-      exit 1
-  esac
+    case $1 in
+        FATAL)   verbosity=$fatal_lvl   ;;
+        ERROR)   verbosity=$error_lvl   ;;
+        WARNING) verbosity=$warning_lvl ;;
+        INFO)    verbosity=$info_lvl    ;;
+        DEBUG)   verbosity=$debug_lvl   ;;
+        TRACE)   verbosity=$trace_lvl   ;;
+        *)
+            echo -e "Error: $0 invalid option '$1'\nTry '$0 --help' for more information.\n"
+            exit 1
+    esac
 }
 
 function usage() {
-  echo -e "`basename $0` v$LOG4SH_VERSION"
-  echo -e "Usage: log4.sh [OPTIONS]"
-  echo -e " -h , --help                    : Print this help"
-  echo -e " -v , --verbosity [LEVEL]       : Define the verbosity level. "
-  echo -e "                                  Level are: FATAL < ERROR < WARNING < INFO < DEBUG < TRACE"
-  echo -e " -d , --dateformat [DATE FORMAT]: Set the date format. Refer to date command (man date)"
-  echo -e " -f , --file [FILE NAME]        : Set the log file name"
-  echo -e "      --version                 : Print version"
-  echo -e ""
-  echo -e "Exit status:"
-  echo -e " 0  if OK,"
-  echo -e " 1  if some problems (e.g., cannot access subdirectory)."
+    echo -e "`basename $0` v$LOG4SH_VERSION"
+    echo -e "Usage: log4.sh [OPTIONS]"
+    echo -e " -h , --help                    : Print this help"
+    echo -e " -v , --verbosity [LEVEL]       : Define the verbosity level. "
+    echo -e "                                  Level are: FATAL < ERROR < WARNING < INFO < DEBUG < TRACE"
+    echo -e " -d , --dateformat [DATE FORMAT]: Set the date format. Refer to date command (man date)"
+    echo -e " -f , --file [FILE NAME]        : Set the log file name"
+    echo -e "      --version                 : Print version"
+    echo -e ""
+    echo -e "Exit status:"
+    echo -e " 0  if OK,"
+    echo -e " 1  if some problems (e.g., cannot access subdirectory)."
 }
 
 OPTS=$(getopt -o :d:v:f:h --long "help,version,verbosity:,file:,dateformat:" -n $LOG4SH_APPNAME -- "$@")
 
 #Bad arguments, something has gone wrong with the getopt command.
 if [ $? -ne 0 ]; then
-  #Option not allowed
-  echo -e "Error: '$0' invalid option '$1'."
-  echo -e "Try '$0 -h' for more information."
-  exit 1
+    #Option not allowed
+    echo -e "Error: '$0' invalid option '$1'."
+    echo -e "Try '$0 -h' for more information."
+    exit 1
 fi
 
 # A little magic, necessary when using getopt.
 eval set -- "$OPTS"
 
 while true; do
-  case "$1" in
-    -h|--help)
-      usage
-      shift;;
-    --version)
-	  echo -e "`basename $0` v$LOG4SH_VERSION"
-	  echo -e "Makes logging in Bash scripting simple"
-	  echo -e "Copyright (c) Marco Lovazzano"
-	  echo -e "Licensed under the GNU General Public License v3.0"
-	  echo -e "http://github.com/martcus"
-      shift;;
-    -v|--verbosity) #Verbosity
-      _set_verbosity "$2"
-      shift 2;;
-    -d|--dateformat) #Date format
-	  date "$2" > /dev/null 2>&1
-	  if [ ! $? -eq 0 ]; then
-	    echo "Error: '$0' '-d $2' is not a valid date format. Refer to date command (man date)"
-		exit 1
-      fi
-      LOG_TIME_FMT=$2
-      shift 2;;
-    -f|--file) #Print to file
-      LOG_FILE=$2
-      shift 2;;
-    --)
-      shift
-      break;;
-  esac
+    case "$1" in
+        -h|--help)
+            usage
+            shift;;
+        --version)
+            echo -e "`basename $0` v$LOG4SH_VERSION"
+            echo -e "Makes logging in Bash scripting simple"
+            echo -e "Copyright (c) Marco Lovazzano"
+            echo -e "Licensed under the GNU General Public License v3.0"
+            echo -e "http://github.com/martcus"
+            shift;;
+        -v|--verbosity) #Verbosity
+            _set_verbosity "$2"
+            shift 2;;
+        -d|--dateformat) #Date format
+            date "$2" > /dev/null 2>&1
+            if [ ! $? -eq 0 ]; then
+                echo "Error: '$0' '-d $2' is not a valid date format. Refer to date command (man date)"
+                exit 1
+            fi
+            LOG_TIME_FMT=$2
+            shift 2;;
+        -f|--file) #Print to file
+            LOG_FILE=$2
+            shift 2;;
+        --)
+            shift
+            break;;
+    esac
 done
